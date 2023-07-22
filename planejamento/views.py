8af0c8be8de7566ae6af8a9b0cdc3e4a777aa293
mardsonferreira <mardsonferreira25@gmail.com>
2023-07-21 23:49:38 -0300

    definir planejamento

---
+++ planejamento/views.py
@@ -0,0 +1,19 @@
+import json
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from perfil.models import Categoria
+
+
+def definir_planejamento(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'definir_planejamento.html', {'categorias': categorias})
+
+@csrf_exempt
+def update_valor_categoria(request, id):
+    novo_valor = json.load(request)['novo_valor']
+    categoria = Categoria.objects.get(id=id)
+    categoria.valor_planejamento = novo_valor
+    categoria.save()
+
+    return JsonResponse({'status': 'Sucesso'})
