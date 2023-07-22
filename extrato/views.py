cdb84701a8bf46f0ae4b05efba9cacdce83c037d
mardsonferreira <mardsonferreira25@gmail.com>
2023-07-21 23:12:24 -0300

    exportar extrato

--- extrato/views.py
+++ extrato/views.py
@@ -1,11 +1,15 @@
+import os
+from io import BytesIO
 from datetime import datetime
 from django.shortcuts import render, redirect
-
-from .models import Valores
-from perfil.models import Categoria, Conta
-
+from django.http import FileResponse
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.template.loader import render_to_string
+from django.conf import settings
+from weasyprint import HTML
+from .models import Valores
+from perfil.models import Categoria, Conta
 
 def novo_valor(request):
     if request.method == "GET":
@@ -62,3 +66,19 @@ def view_extrato(request):
         valores = valores.filter(categoria__id=categoria_get)
 
     return render(request, 'view_extrato.html', {'valores': valores, 'contas': contas, 'categorias': categorias})
+
+def exportar_pdf(request):
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+    
+    path_template = os.path.join(settings.BASE_DIR, 'templates/partials/extrato.html')
+    path_output = BytesIO()
+
+    template_render = render_to_string(path_template, {'valores': valores, 'contas': contas, 'categorias': categorias})
+    HTML(string=template_render).write_pdf(path_output)
+
+    path_output.seek(0)
+    
+
+    return FileResponse(path_output, filename="extrato.pdf")
